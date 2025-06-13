966c7a8a53cfa0c82e22eb69a7ee233302a21fea
Salvatore Ingala <6681844+bigspider@users.noreply.github.com>
2025-06-13 19:55:58 +0200

    Demo step 2: Rock-Paper-Scissors

--- apps/demo/client/src/lib.rs
+++ apps/demo/client/src/lib.rs
@@ -2,9 +2,16 @@ use sdk::{
     comm::{self, send_message, SendMessageError},
     vanadium_client::{VAppExecutionError, VAppTransport},
 };
+use serde::{Deserialize, Serialize};
 
 pub struct DemoClient {
     app_transport: Box<dyn VAppTransport + Send>,
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+pub enum Command {
+    Commit { c_a: [u8; 32] },
+    Reveal { m_a: u8, r_a: [u8; 32] },
 }
 
 impl DemoClient {
@@ -12,17 +19,37 @@ impl DemoClient {
         Self { app_transport }
     }
 
-    pub async fn echo(&mut self, data: &[u8]) -> Result<Vec<u8>, Box<dyn core::error::Error>> {
-        Ok(comm::send_message(&mut self.app_transport, &data).await?)
+    pub async fn commit(&mut self, c_a: [u8; 32]) -> Result<u8, Box<dyn core::error::Error>> {
+        let command = Command::Commit { c_a };
+        let msg = postcard::to_allocvec(&command).map_err(|_| "Serialization failed")?;
+        let response = comm::send_message(&mut self.app_transport, &msg).await?;
+        if response.len() != 1 || response[0] > 2 {
+            return Err("Invalid response from the app".into());
+        }
+        Ok(response[0])
     }
 
-    pub async fn exit(&mut self) -> Result<i32, &'static str> {
-        match send_message(&mut self.app_transport, &[]).await {
-            Ok(_) => Err("Exit message shouldn't return!"),
+    pub async fn reveal(
+        &mut self,
+        m_a: u8,
+        r_a: [u8; 32],
+    ) -> Result<u8, Box<dyn core::error::Error>> {
+        let command = Command::Reveal { m_a, r_a };
+        let msg = postcard::to_allocvec(&command).map_err(|_| "Serialization failed")?;
+        let response = comm::send_message(&mut self.app_transport, &msg).await?;
+        if response.len() != 1 || response[0] > 2 {
+            return Err("Invalid response from the app".into());
+        }
+        Ok(response[0])
+    }
+
+    pub async fn exit(&mut self) -> Result<i32, Box<dyn core::error::Error>> {
+        match comm::send_message(&mut self.app_transport, &[]).await {
+            Ok(_) => Err("Exit message shouldn't return!".into()),
             Err(SendMessageError::VAppExecutionError(VAppExecutionError::AppExited(code))) => {
                 Ok(code)
             }
-            Err(_) => Err("Unexpected error"),
+            Err(_) => Err("Unexpected error".into()),
         }
     }
 }
