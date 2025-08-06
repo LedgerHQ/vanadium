86e0f0be39497ce1a8d2aab9fa47d6bc26bf7831
Salvatore Ingala <6681844+bigspider@users.noreply.github.com>
2025-08-06 15:31:11 +0200

    Dummy slip-21 derivation
    To be reverted once the authorized SLIP-21 paths work correctly
    in apps with the Ledger Rust SDK.

--- vm/src/handlers/lib/ecall/slip21.rs
+++ vm/src/handlers/lib/ecall/slip21.rs
@@ -33,19 +33,24 @@ fn get_seed() -> [u8; 32] {
     let mut path = Vec::with_capacity(SEED_MASTER_PATH.len() + 1);
     path.push(0x00);
     path.extend_from_slice(SEED_MASTER_PATH.as_bytes());
-    let mut seed = [0u8; 32];
-    unsafe {
-        sys::os_perso_derive_node_with_seed_key(
-            sys::HDW_SLIP21,
-            sys::CX_CURVE_SECP256K1,
-            path.as_ptr() as *const u32,
-            path.len() as u32,
-            seed.as_mut_ptr(),
-            core::ptr::null_mut(),
-            core::ptr::null_mut(),
-            0,
-        );
-    }
+    // let mut seed = [0u8; 32];
+    // unsafe {
+    //     sys::os_perso_derive_node_with_seed_key(
+    //         sys::HDW_SLIP21,
+    //         sys::CX_CURVE_SECP256K1,
+    //         path.as_ptr() as *const u32,
+    //         path.len() as u32,
+    //         seed.as_mut_ptr(),
+    //         core::ptr::null_mut(),
+    //         core::ptr::null_mut(),
+    //         0,
+    //     );
+    // }
+
+    // TODO: remove this once the Rust SDK has proper SLIP-21 path support
+    // We use a dummy seed for testing purposes.
+    let mut seed = [42u8; 32];
+
     seed
 }
 
