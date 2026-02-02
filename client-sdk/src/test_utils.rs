6d56ec198740a533d209a107a1b09b667337ecb4
Salvatore Ingala <6681844+bigspider@users.noreply.github.com>
2026-02-02 16:59:01 +0100

    Vanadium client: separate client creation from engine running
    It makes sense in some cases to run a Vanadium client without
    immediately launching a V-App, or keeping it running after the
    V-App exits.

--- client-sdk/src/test_utils.rs
+++ client-sdk/src/test_utils.rs
@@ -209,13 +209,12 @@ where
 
     TestSetup::new(vanadium_binary, |transport| async move {
         let print_writer = Box::new(FileLineWriter::new("print.log", true, true));
-        let vanadium_client =
-            VanadiumAppClient::new(vapp_binary, transport, print_writer)
-                .await
-                .expect(&format!(
-                    "Failed to create client for vapp binary: {}",
-                    vapp_binary
-                ));
+        let vanadium_client = VanadiumAppClient::with_vapp(vapp_binary, transport, print_writer)
+            .await
+            .expect(&format!(
+                "Failed to create client for vapp binary: {}",
+                vapp_binary
+            ));
 
         create_client(Box::new(vanadium_client))
     })
