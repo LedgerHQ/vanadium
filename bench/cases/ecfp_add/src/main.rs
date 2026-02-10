ddc5f4c08c983b17c5bce9b63cbc844d17f56ff2
Salvatore Ingala <6681844+bigspider@users.noreply.github.com>
2026-02-10 22:23:42 +0100

    Add benchmarks for Secp256k1 point addition and scalarmul

---
+++ bench/cases/ecfp_add/src/main.rs
@@ -0,0 +1,33 @@
+// This benchmark measures the addition of points in the Secp256k1 curve
+
+#![cfg_attr(feature = "target_vanadium_ledger", no_std, no_main)]
+
+use hex_literal::hex;
+use sdk::curve::Secp256k1Point;
+
+extern crate alloc;
+
+sdk::bootstrap!();
+
+pub fn main() {
+    let msg: [u8; 8] = sdk::xrecv(8).try_into().expect("Expected 8 bytes");
+    let n_reps = u64::from_be_bytes(msg);
+
+    let mut p = Secp256k1Point::new(
+        hex!("c6047f9441ed7d6d3045406e95c07cd85c778e4b8cef3ca7abac09b95c709ee5"),
+        hex!("1ae168fea63dc339a3c58419466ceaeef7f632653266d0e1236431a950cfe52a"),
+    );
+    let q = Secp256k1Point::new(
+        hex!("f9308a019258c31049344f85f89d5229b531c845836f99b08601f113bce036f9"),
+        hex!("388f7b0f632de8140fe337e62a37f3566500a99934c2231b6cb9fd7584b8e672"),
+    );
+
+    //repeatedly add q to p
+    for _ in 0..n_reps {
+        p = &p + &q;
+    }
+
+    core::hint::black_box(p);
+
+    sdk::exit(0);
+}
