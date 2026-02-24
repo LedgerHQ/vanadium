8539a020d3bf709333d3fd3e62aa4d82105f8065
Salvatore Ingala <6681844+bigspider@users.noreply.github.com>
2026-02-24 16:17:53 +0100

    Delete unused trait (duplicated elsewhere)

--- vm/src/handlers/lib/mod.rs
+++ vm/src/handlers/lib/mod.rs
@@ -1,17 +1,3 @@
-use common::client_commands::Message;
-
 pub mod ecall;
 pub mod evict;
 pub mod outsourced_mem;
-
-trait SerializeToComm<const N: usize> {
-    fn serialize_to_comm(&self, response: &mut ledger_device_sdk::io::CommandResponse<'_, N>);
-}
-
-impl<'a, T: Message<'a>, const N: usize> SerializeToComm<N> for T {
-    fn serialize_to_comm(&self, response: &mut ledger_device_sdk::io::CommandResponse<'_, N>) {
-        self.serialize_with(|data| {
-            response.append(data).unwrap();
-        });
-    }
-}
