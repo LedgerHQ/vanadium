98e9340a30817f46c51a4bf9216e3301a0add9b1
Salvatore Ingala <6681844+bigspider@users.noreply.github.com>
2026-03-09 17:21:24 +0100

    Re-export types in vnd-bitcoin-common if useful for the client

--- apps/bitcoin/client/src/lib.rs
+++ apps/bitcoin/client/src/lib.rs
@@ -3,3 +3,9 @@ extern crate bitcoin;
 mod client;
 
 pub use client::BitcoinClient;
+
+// Re-export from the sdk
+pub use sdk::vanadium_client::{client_utils::*, VAppTransport};
+
+// Re-exports from the `common` module that are useful for users of this library.
+pub use common::{bip388, message, por::ProofOfRegistration, psbt::psbt_v0_to_v2};
