d43211305b66580e0104adf334a35dde7d85cdc8
Salvatore Ingala <6681844+bigspider@users.noreply.github.com>
2026-03-19 14:49:42 +0100

    Re-export all types used in the client of the bitcoin V-app

--- apps/bitcoin/client/src/lib.rs
+++ apps/bitcoin/client/src/lib.rs
@@ -8,4 +8,10 @@ pub use client::BitcoinClient;
 pub use sdk::vanadium_client::{client_utils::*, VAppTransport};
 
 // Re-exports from the `common` module that are useful for users of this library.
-pub use common::{bip388, message, por::ProofOfRegistration, psbt::psbt_v0_to_v2};
+pub use common::{
+    bip388::{self, WalletPolicy},
+    identity::{self, IdentityKey},
+    message::{self, IdentitySignature, RegisteredIdentityEntry},
+    por::{ProofOfRegistration, RegistrationId},
+    psbt::psbt_v0_to_v2,
+};
